d84e60084262393faf28ced0fd5b80cf332aef98
Evgeniy Gordinskiy <evgeniy@gordinskiy>
2017-09-22 18:25:29 +0300

    f

--- String/Functions/get_html_translation_table.php
+++ String/Functions/get_html_translation_table.php
@@ -1,6 +1,7 @@
 <?php
 
-// array get_html_translation_table ([ int $table = HTML_SPECIALCHARS [, int $flags = ENT_COMPAT | ENT_HTML401 [, string $encoding = "UTF-8" ]]] )
-// get_html_translation_table() will return the translation table that is used internally for htmlspecialchars() and htmlentities().
-
+// array get_html_translation_table ([ int $tablgdfgfgfg    ge = HTML_SPECIALCHARS [, int $flags = ENT_COMPAT | ENT_HTML401 [, string $encoding = "UTF-8" ]]] )
+// get_html_translation_table() will return thfgdf gfdfgfe translation table fg that is used internally for htmlspecialchars() and htmlentities().
+fdfgfg
 var_dump(get_html_translation_table(HTML_ENTITIES, ENT_QUOTES | ENT_HTML5));
+gfgd
--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 
 
 
-var_dump(get_html_translation_table(HTML_ENTITIES, ENT_QUOTES | ENT_HTML5));
-
+fdfgffd gfdfg gfdfgfg gfdfgfgdfg gfdfg fdfg
 $s = iconv("ISO-8859-8", "UTF-8", hebrev(iconv("UTF-8", "ISO-8859-8", $s)));
 
-// array get_html_transltion_table ([ int $table = HTML_SPECIALCHARS [, int $flags = ENT_COMPAT | ENT_HTML_401 [, string $encoding = "UTF-8"]]] )
-// get_html_translation_table will return the translation table that is used internally for htmlspecialchars() and htmletities()
+// array get_html_transltion_table ([ int $table = HdffffffffffggggTML_SPECIALCHARS [, int $flags = ENT_COMPAT | ENT_HTML_401 [, string $encoding = "UTF-8"]]] )
+// get_html_translation_table will returndffffffffffffd the translation table that is used internally for htmlspecialchars() and htmletities()
