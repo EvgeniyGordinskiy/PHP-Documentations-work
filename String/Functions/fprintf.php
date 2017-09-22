5ade6a8cf3b6ea903ba4d4b4b342408c7211a57d
Evgeniy Gordinskiy <evgeniy@gordinskiy>
2017-09-22 17:58:29 +0300

    Edit fprintf.

--- String/Functions/fprintf.php
+++ String/Functions/fprintf.php
@@ -2,3 +2,10 @@
 
 // int fprintf ( resource $handle , string $format [, mixed $args [, mixed $... ]] )
 // Write a string produced according to format to the stream resource specified by handle.
+
+if (!($fp = fopen('date.txt', 'w'))) {
+    return;
+}
+
+fprintf($fp, "%04d-%02d-%02d", $year, $month, $day);
+// запишет форматированную ISO дату в файл date.txt
