04d7a51843889b0654bb2f5acd0992bbfd2f7da7
EvgeniyGordinskiy <evgeniy@gordinskiy.com>
2017-12-05 14:26:14 +0200

    Added array_intersect, array_intersect_ukey, array_key_exist, array_keys, array_map.

--- Array/Functions/array_product.php
+++ Array/Functions/array_product.php
@@ -1,14 +1,15 @@
 <?php
 
-//array_product — Calculate the product of values in an array
+// array_product — Calculate the product of values in an array
 
 // number array_product ( array $array )
 
-//  array_product() returns the product of values in an array.
+// array_product() returns the product of values in an array.
 
-$check[] = outbool(TRUE);
-$check[] = outbool(1);
-$check[] = outbool(FALSE);
-$check[] = outbool(0);
+array_product([2, 2, 2]); // 8
 
-array_product($check); // false
+array_product([2, 2, 2, false]); // 0
+
+array_product([2, 2, 2, '']); // 0
+
+array_product([2, 2, 2, [] ]); // 8
